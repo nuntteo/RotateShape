0ed26121fb447ded1eaafb7def0aa9c0170cbdc9
nuntteo <101611219+nuntteo@users.noreply.github.com>
2022-04-12 09:31:12 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(10, 20, 30, 30);
+  noFill();
+  
+  translate(width / 2, height / 2);
+  
+  for(var i = 0; i<100; i++){
+    push()
+    
+    rotate(sin(frameCount + i) * 100);
+    
+    var r = map(sin(frameCount), -1, 1, 50, 255);
+    var g = map(cos(frameCount / 2), -1, 1, 50, 255);
+    var b = map(sin(frameCount / 4), -1, 1, 50, 255);
+    
+    stroke(r, g, b);
+    
+    rect(0, 0, 600 - i * 3, 600 - i * 3, 100 - i);
+    pop();
+  }
+}
